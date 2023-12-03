aabd891dfd6525c4830446f7e0f92f5df3feca66
Gunjan56 <gangwargunjan9@gmail.com>
2023-12-03 14:36:28 +0530

    update code

---
+++ script.js
@@ -0,0 +1,26 @@
+const box = document.querySelector(".box");
+// let date = new Date();
+// box.innerHTML = date.toLocaleTimeString(); no need to do this
+// console.log(date);
+
+// script is running only one time
+// date is not updating becoz this script runs only once
+
+// use setinterval
+// now this function will be call every 1 second whic will update ui
+// let see our console
+// now just use dom
+setInterval(() => {
+  let date = new Date().toLocaleTimeString();
+  box.innerHTML = date;
+}, 1000);
+
+// check type of date
+// arey tumse he bol rha hu 
+// jab locale string phele se he string hai why are you using upper case and to string
+
+// git setup?
+//not done do help
+
+// easy peasy lemon 
+
